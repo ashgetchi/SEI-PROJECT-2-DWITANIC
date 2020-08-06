ba28c7d52626bbde6141fde62076821ef9a00676
Ash Atighetchi <ashgetchi@gmail.com>
2020-08-06 12:34:23 +0100

    got the quotes to go on screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch , Route } from 'react-router-dom'
 import Home from './components/Home'
 import GamePage from './components/GamePage'
+
 console.log(process.env.REACT_APP_MY_API_KEY)
 const App = () => {
   return (
--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,7 +1,27 @@
 import React from 'react' 
-const GamePage = () => {
-  return (
-    <h1>GAME</h1>
-  )
+import axios from 'axios'
+class GamePage extends React.Component {
+  state = {
+    quotes: '',
+    character: ''
+  }
+  async componentDidMount() {
+    try {
+      const res = await axios.get('https://cors-anywhere.herokuapp.com/https://officeapi.dev/api/quotes/random')
+      this.setState({ quotes: res.data.data.content, character: res.data.data.character.firstname })
+      console.log(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  render() {
+    return (
+      <>
+        <h5>Quote: </h5>
+        <h4>' {this.state.quotes} '</h4>
+        <h4> {this.state.character}</h4>
+      </>
+    )
+  }
 }
 export default GamePage
